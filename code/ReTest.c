a256be9426fb0561fb4c8e1faa4aa7d2270455ef
Mats Engstrom <mats.engstrom@gmail.com>
2016-10-25 00:31:48 +0800

    Updates & Restructures

--- code/ReTest.c
+++ code/ReTest.c
@@ -19,8 +19,8 @@
 #include "lcd.h"
 #include "rotaryenc.h"
 
-//#include "fonts/FreeSans9pt7b.h"
-#include "fonts/FreeSansBold9pt7b.h"
+// #include "fonts/FreeSans9pt7b.h"
+// LcdSetFont(&FreeSansBold9pt7b);
     
 //
 // D8   PB0 
@@ -50,13 +50,25 @@
 // D7   PD7          < Relay NC pin
 //
 
+#define SLEEP(MS)     tickMs=0; while (tickMs<(MS)){}
+
 #define RELAY_COIL_PIN    PC0
-#define BUT1_STATUS       !(PINC & (1<<PC1))    // Push button
+#define QUAD_BUTTON       !(PINC & (1<<PC1))    // Encoder button
+#define BUT1_STATUS       !(PINC & (1<<PC3))    // Push button
 #define BUT2_STATUS       !(PINC & (1<<PC2))    // Push button
-#define QUAD_BUTTON       !(PINC & (1<<PC3))    // Encoder button
 #define QUAD_B_STATUS     !(PINC & (1<<PC4))    // Encoder Out1
 #define QUAD_A_STATUS     !(PINC & (1<<PC5))    // Encoder Out2
+
+#define GRAPHHEIGTH 4
+
+#define HIGH 0x8000
+#define MID  0xC000
+#define LOW  0x4000
+
 #define RELAY_NONC_STATUS (PIND & ((1<<PD6)|(1<<PD7)))
+
+#define RELAYOFF           (PORTC|=(1<<PC0))
+#define RELAYON            (PORTC&=(~(1<<PC0)))
 
 //
 // databits  ???? ???? ???? ??dd
@@ -67,8 +79,11 @@
 // With a tick rate at 10uS the maximum time is 
 // 16383*10us=163830us=163mS
 //
-volatile uint16_t samples[512];
-volatile uint16_t sampleCnt;
+
+#define MAXSAMPLES  255
+volatile uint8_t sampleCnt;
+volatile uint16_t samples1[MAXSAMPLES];
+volatile uint16_t samples2[MAXSAMPLES];
 
 volatile uint16_t tickMs;         // RTC updated by Timer2
 volatile uint16_t encValue;       // Rotary Encoder value upd by Timer2
@@ -83,21 +98,40 @@ volatile uint16_t speed;
 // a timestamp in the relay sample/event table if
 // any change in the pins are detected
 //
+// sampleCnt is used as a flag for "Start" and "Done" when set 
+// to 0x00 and 0xFF (MAXSAMPLES) for respective function
+//
 ISR (TIMER0_COMPA_vect) {  
   static uint16_t tick;
   static uint8_t oldPins=0;
   uint8_t pins;
+  uint16_t pins16;
 
-  if (sampleCnt==512) {
+  // If stopped then just exit
+  if (sampleCnt==MAXSAMPLES) return;
+
+  // If just started then store an initial reading and exit
+  if (sampleCnt==0) {
     tick=0;
-  } else {
     pins=RELAY_NONC_STATUS;
-    if (pins!=oldPins) {
-      oldPins=pins;
-      samples[sampleCnt]=tick|(pins<<14);
-      sampleCnt++;
-    }
-    tick++;
+    oldPins=pins;
+    pins16=pins<<8;
+    samples1[sampleCnt++]=tick|pins16;
+    return;
+  }
+
+  // If not first time then increment tick, and if pins has 
+  // changed since last reading then store the sample
+  tick++;
+  pins=RELAY_NONC_STATUS;
+  if (pins!=oldPins) {
+    oldPins=pins;
+    pins16=pins<<8;
+    samples1[sampleCnt++]=tick|pins16;
+  }
+  // If enough time has elapsed then signal beging ready
+  if (tick>5000) { // 5000 ticks = 50 000 us = 50ms
+    sampleCnt=MAXSAMPLES;
   }
 }
 
@@ -139,98 +173,7 @@ ISR (TIMER2_COMPA_vect, ISR_NOBLOCK) {
     }
 }
 
-#define HIGH 0x8000
-#define MID  0x8000+0x4000
-#define LOW  0x4000
 
-void FakeDataOff() {
-  uint16_t p;
-  uint16_t t;
-  uint8_t i;
-  memset((void *)samples,0,sizeof(samples));
-  p=0;
-  t=0;
-  samples[p++]=HIGH+t;
-
-  // After a random time go middle
-  t+=4*100+(rand()%(2*100));
-  samples[p++]=MID+t;
-
-  // Bounce between HIGH and MID for a while
-  for (i=0; i<1+rand()%3; i++) {
-    t+=10+(rand()%(100));
-    samples[p++]=HIGH+t;
-    t+=10+(rand()%(100));
-    samples[p++]=MID+t;
-  }
-
-  // Stay at MID for a while
-  t+=100+(rand()%(300));
-
-  // Bounce between LOW and MID for a while
-  for (i=0; i<2+rand()%10; i++) {
-    t+=10+(rand()%(100));
-    samples[p++]=LOW+t;
-    t+=10+(rand()%(100));
-    samples[p++]=MID+t;
-  }
-
-  // Finish up at LOW
-    t+=10+(rand()%(100));
-    samples[p++]=LOW+t;
-  // Finish up at LOW
-    t+=10+(rand()%(100));
-    samples[p++]=LOW+t;
-  // Finish up at LOW
-    t+=10+(rand()%(100));
-    samples[p++]=LOW+t;
-}
-
-
-void FakeDataOn() {
-  uint16_t p;
-  uint16_t t;
-  uint8_t i;
-  memset((void *)samples,0,sizeof(samples));
-  p=0;
-  t=0;
-  samples[p++]=LOW+t;
-
-  // After a random time go middle
-  t+=4*100+(rand()%(2*100));
-  samples[p++]=MID+t;
-
-  // Bounce between LOW and MID for a while
-  for (i=0; i<1+rand()%3; i++) {
-    t+=10+(rand()%(100));
-    samples[p++]=LOW+t;
-    t+=10+(rand()%(100));
-    samples[p++]=MID+t;
-  }
-
-  // Stay at MID for a while
-  t+=400+(rand()%(100));
-
-  // Bounce between HIGH and MID for a while
-  for (i=0; i<2+rand()%10; i++) {
-    t+=10+(rand()%(100));
-    samples[p++]=HIGH+t;
-    t+=10+(rand()%(100));
-    samples[p++]=MID+t;
-  }
-
-  // Finish up at HIGH
-    t+=10+(rand()%(100));
-    samples[p++]=HIGH+t;
-  // Finish up at HIGH
-    t+=10+(rand()%(100));
-    samples[p++]=HIGH+t;
-  // Finish up at HIGH
-    t+=10+(rand()%(100));
-    samples[p++]=HIGH+t;
-}
-
-#define GRAPHHEIGTH 4
 //
 //
 //
@@ -280,44 +223,66 @@ void PlotLine(uint8_t x, uint8_t yBase, uint16_t v, uint16_t lastV) {
     return;
   }
 }
-//8550
 
-///
-///
-///
-void PlotGraph(uint8_t yBase, uint16_t *time1, uint16_t *time2) {
-  uint8_t x;
-  uint16_t p;
-  uint16_t v,lastV;
+
+//
+// Scans through the sample and finds out the times until the first
+// and last events. I.E how long it took for the relay to start moving
+// and how long it took until fully stable.
+//
+// Returns a suggestion for scaling factor for the plotting of data onto 
+// the LCD.
+//
+// Also return the start and stable times in 0.1us resolution for easier
+// printing.
+//
+uint16_t AnalyseSamples(uint16_t *samples, uint16_t *time1, uint16_t *time2) {
+  uint8_t i;
+  uint16_t *p;
   uint16_t maxt;
-  uint16_t t,t1,t2;
+  uint16_t t;
   uint16_t factor;
 
+  *time1=0;     // Quench compiler warning
+
   maxt=0;
-  for (p=0; p<512; p++) {
-    t=samples[p]&0x3FFF;
+  p=samples;
+  for (i=0; i<MAXSAMPLES; i++) {
+    t=(*p)&0x3FFF;
+    if (i==1) *time1=t/10;
+    p++;
     if (t>maxt) {
       maxt=t;
     }
   }
-  *time1=samples[1]&0x3FFF;
-  *time2=maxt;
+  *time2=maxt/10;
+  factor=(maxt+maxt/10)/84;
+  return factor;
+}
 
-  maxt=1900;
-  factor=((maxt+maxt/5)/84);
+
+//
+//
+//
+void PlotGraph(uint16_t *samples, uint8_t yBase, uint16_t factor) {
+  uint8_t x;
+  uint16_t *p;
+  uint16_t v,lastV;
+  uint16_t t,t1,t2;
+  uint16_t i;
 
   LcdDrawLine(0,yBase-GRAPHHEIGTH-1, 0,yBase+GRAPHHEIGTH+1, BLACK);
   LcdDrawLine(83,yBase-GRAPHHEIGTH-1, 83,yBase+GRAPHHEIGTH+1, BLACK);
 
-  for (p=0; p<511; p++) {
-    v=samples[p]&0xC000;
-    if (p==0) lastV=v;
-    t1=samples[p]&0x3FFF;
-    t2=samples[p+1]&0x3FFF;
+  p=samples;
+  for (i=0; i<MAXSAMPLES; i++) {
+    v=(*p)&0xC000;
+    if (i==0) lastV=v;
+    t1=(*p)&0x3FFF;
+    t2=(*(p+1))&0x3FFF;
     if (t2==0) {
-      p=512;        // Exits the outer loop at next iteration
+      i=MAXSAMPLES; // Exits the outer loop at next iteration
       t2=83*factor; // Make sure the last value goes all the way
-      v=lastV;
     }
     for (t=t1; t<t2; t++){
       x=t/factor;
@@ -325,19 +290,54 @@ void PlotGraph(uint8_t yBase, uint16_t *time1, uint16_t *time2) {
       PlotLine(x,yBase,v,lastV);
       lastV=v;
     }
+    p++;
   }
-
 }
 
 
-int main(void) {
+void DrawGraph(uint16_t *samples, uint8_t y, uint16_t useFactor, char *caption) {
+    char s[20];
+    uint16_t factor;
+    uint16_t time1,time2;
+    factor=AnalyseSamples((uint16_t *)samples, &time1, &time2);
+    if (useFactor!=0) factor=useFactor; 
+    PlotGraph(samples, y, factor);
+    setCursor(0,y+8);
+    sprintf(s,"%s %2d.%d/%2d.%d",caption,time1/10,time1%10,time2/10,time2%10);
+    LcdPrint(s);
+}
 
+//
+// Stop the sampling engine by setting the sample counter
+// to its maximium value. Also stop the 10us timer
+//
+void StopSampler(void) {
+  sampleCnt=MAXSAMPLES;
+  TCCR0B &= ~((1<<CS00)|(1<<CS01)|(1<<CS02)); 
+}
+
+
+//
+// Set sample counter to 0 to indicate a new set 
+// of samples. Also start the 10us timer.
+//
+void StartSampler(void) {
+  memset((uint16_t *)samples1,0,sizeof(samples1));
+  sampleCnt=0;
+  TCCR0B |= (1<<CS00);    // No Prescaler, Start the timer
+}
+
+
+//
+//
+//
+int main(void) {
   // Setup Timer0 to generate 10us interrupts
-  // used for sampling relay
+  // used for sampling the relay, but don't start it
   TCCR0A |= (1 << WGM01);   // Timer Mode CTC
   OCR0A = 160;              // 16MHz/160 ticks = 10us
   TIMSK0 |= (1 << OCIE0A);  // Set the ISR COMPA vect
-  TCCR0B |= (1 << CS00);    // No Prescaler, Start the timer
+  TCCR0B &= ~((1<<CS00)|(1<<CS01)|(1<<CS02)); // Timer is stopped
 
   // Setup Timer2 to generate 1ms interrupts
   // used for RTC and user interface
@@ -350,53 +350,41 @@ int main(void) {
   
   DDRC=(1<<PC0);  // PC0 drives the relay (inverted)
   PORTC=0x3F;     // Turns on pullup on PC1..PC5 and deactivates Relay 
+  PORTD=0XC0;     // Turns on pullup on PD6..PD7
 
-  encMin=10;
-  encMax=10000;
-  encValue=1000;
+  RELAYOFF;
+
+  encMin=1;
+  encMax=150;
+  encValue=10;
 
   SpiInit();
-  LcdInit(40);
+  LcdInit(50);
   LcdRefresh();
-  _delay_ms(100);
 
-  LcdSetFont(NULL);
-  LcdSetFont(&FreeSansBold9pt7b);
-  setTextSize(1);
-  LcdRefresh();
-  _delay_ms(500);
-
+//  LcdSetFont(&FreeSansBold9pt7b);
   LcdSetFont(NULL);
   setTextSize(1);
   setTextColor(BLACK,WHITE);
 
   for(;;) {
-    char s[10];
-    uint16_t t1,t2;
-
     LcdClear();
 
-    FakeDataOn();
-    PlotGraph(5,&t1,&t2);
-    setCursor(0,5+8);
-    t1/=10;
-    t2/=10;
-    sprintf(s,"On  %2d.%d/%2d.%d",t1/10,t1%10,t2/10,t2%10);
-    LcdPrint(s);
+    SLEEP(100);
+    RELAYON;
+    StartSampler();
+    while(sampleCnt!=MAXSAMPLES);
+    StopSampler();
+    DrawGraph((uint16_t *)samples1,5,encValue,"On ");
 
-    FakeDataOff();
-    PlotGraph(30,&t1,&t2);
-    setCursor(0,30+8);
-    t1/=10;
-    t2/=10;
-    sprintf(s,"Off %2d.%d/%2d.%d",t1/10,t1%10,t2/10,t2%10);
-    LcdPrint(s);
-
-    _delay_ms(500);
+    SLEEP(100);
+    RELAYOFF;
+    StartSampler();
+    while(sampleCnt!=MAXSAMPLES);
+    DrawGraph((uint16_t *)samples1,30,encValue,"Off");
 
     LcdRefresh();
-  }
-
+   }
 }
 
 
